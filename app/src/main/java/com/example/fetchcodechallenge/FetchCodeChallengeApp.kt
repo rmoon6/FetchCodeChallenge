cf76d6d053a8d81156a7003b7a05013dc0e38914
Chase <chase.moon93@gmail.com>
2024-10-11 12:01:57 -0700

    Moved some code from the application to the retrofit interface

--- app/src/main/java/com/example/fetchcodechallenge/FetchCodeChallengeApp.kt
+++ app/src/main/java/com/example/fetchcodechallenge/FetchCodeChallengeApp.kt
@@ -2,45 +2,13 @@ package com.example.fetchcodechallenge
 
 import android.app.Application
 import com.example.fetchcodechallenge.api.FetchCodeChallengeApi
-import com.squareup.moshi.Moshi
-import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
-import okhttp3.OkHttpClient
-import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
 
 class FetchCodeChallengeApp : Application() {
 
-    companion object {
-        private const val BASE_API_URL = "https://fetch-hiring.s3.amazonaws.com/"
-    }
-
-    lateinit var moshi: Moshi
     lateinit var api: FetchCodeChallengeApi
 
     override fun onCreate() {
         super.onCreate()
-        moshi = createMoshi()
-
-        val retrofit = createRetrofit(okHttpClient = createOkHttpClient(), moshi = moshi)
-        api = FetchCodeChallengeApi.createInstance(retrofit)
-    }
-
-    private fun createMoshi(): Moshi {
-        return Moshi.Builder()
-            .add(KotlinJsonAdapterFactory())
-            .build()
-    }
-
-    private fun createOkHttpClient(): OkHttpClient {
-        return OkHttpClient.Builder().build()
-    }
-
-    private fun createRetrofit(okHttpClient: OkHttpClient, moshi: Moshi): Retrofit {
-        return Retrofit.Builder()
-            .baseUrl(BASE_API_URL)
-            .client(okHttpClient)
-            .addConverterFactory(MoshiConverterFactory.create(moshi))
-            .build()
+        api = FetchCodeChallengeApi.createInstance()
     }
 }
-
--- app/src/main/java/com/example/fetchcodechallenge/api/FetchCodeChallengeApi.kt
+++ app/src/main/java/com/example/fetchcodechallenge/api/FetchCodeChallengeApi.kt
@@ -1,6 +1,10 @@
 package com.example.fetchcodechallenge.api
 
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 
 interface FetchCodeChallengeApi {
@@ -9,8 +13,33 @@ interface FetchCodeChallengeApi {
     suspend fun getItems(): List<FetchListItem>
 
     companion object {
-        fun createInstance(retrofit: Retrofit): FetchCodeChallengeApi {
+
+        private const val BASE_API_URL = "https://fetch-hiring.s3.amazonaws.com/"
+
+        fun createInstance(): FetchCodeChallengeApi {
+            val retrofit = createRetrofit(
+                okHttpClient = createOkHttpClient(),
+                moshi = createMoshi()
+            )
             return retrofit.create(FetchCodeChallengeApi::class.java)
+        }
+
+        private fun createMoshi(): Moshi {
+            return Moshi.Builder()
+                .add(KotlinJsonAdapterFactory())
+                .build()
+        }
+
+        private fun createOkHttpClient(): OkHttpClient {
+            return OkHttpClient.Builder().build()
+        }
+
+        private fun createRetrofit(okHttpClient: OkHttpClient, moshi: Moshi): Retrofit {
+            return Retrofit.Builder()
+                .baseUrl(BASE_API_URL)
+                .client(okHttpClient)
+                .addConverterFactory(MoshiConverterFactory.create(moshi))
+                .build()
         }
     }
 }
